1ad719d64866dfc0d7b2e8b9f4e692da232de5cc
Kiiro <ajxx030217@163.com>
2025-02-13 23:52:49 -0500

    feat: Part13 (8)

---
+++ Part13/controllers/users.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.put('/:username', async (req, res) => {
+  const { username } = req.params
+  const user = await User.findOne({
+    where: {
+      username
+    }
+  })
+
+  if (user) {
+    user.name = req.body.name
+    await user.save()
+    return res.json(user)
+  }
+
+  return res.status(404).end()
+})
+
+module.exports = router

2e57055a3c01815105a87166d0ba026decb6a212
Kiiro <ajxx030217@163.com>
2025-02-16 20:35:27 -0500

    feat: Part13 (22)

--- Part13/controllers/reading_lists.js
+++ Part13/controllers/reading_lists.js
@@ -4,14 +4,19 @@ const { tokenExtractor } = require('../util/middleware')
 const router = require('express').Router()
 
 router.post('/', tokenExtractor, async (req, res) => {
-  // const { blogId, userId } = req.body
-  // console.log('req.body', req.body)
-
-  // const user = await User.findByPk(req.decodedToken.id)
-  // const blog = await Blog.findByPk(blogId)
-
   const readingList = await ReadingList.create(req.body)
   res.json(readingList)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id)
+  if (readingList) {
+    readingList.read = req.body.read
+    await readingList.save()
+    res.json(readingList)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
